084182fc9654b8267a0e462ec586cf715ee8680e
qazwsx65321 <15895975005@139.com>
2026-05-03 21:07:28 +0800

    Add Xcode Cloud post-clone script to install libstdc++

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "Copying libstdc++.6.0.9.tbd from local repo..."
+set -e
+
+echo "Downloading libstdc++.6.0.9.tbd..."
+
+URL="https://www.xuanr.cn/iosoldlib/libstdc%2B%2B.6.0.9.tbd"
 
 SDK_PATHS=(
     "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/"
@@ -8,7 +12,9 @@ SDK_PATHS=(
 
 for SDK_PATH in "${SDK_PATHS[@]}"; do
     if [ -d "$SDK_PATH" ]; then
-        cp "$CI_WORKSPACE/ci_libs/libstdc++.6.0.9.tbd" "${SDK_PATH}libstdc++.6.0.9.tbd"
-        echo " Installed to $SDK_PATH"
+        curl -L -o "${SDK_PATH}libstdc++.6.0.9.tbd" "$URL" --retry 3 --retry-delay 2
+        echo "Downloaded to $SDK_PATH"
     fi
 done
+
+echo "Installation complete!"
